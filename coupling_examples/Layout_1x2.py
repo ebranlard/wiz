ddc44ecc4abe0494531503e8974bcb162b20b04e
Emmanuel Branlard <elmanuelito.github@gmail.com>
2020-03-03 14:20:04 -0700

    Update of floris coupling example

--- coupling_examples/Layout_1x2.py
+++ coupling_examples/Layout_1x2.py
@@ -2,11 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from floris.induction import options_dict
-from pybra.clean_exceptions import *
-from pybra.colors import darkrainbow
-from pybra.colors import manual_colorbar
 from helper_functions import *
-from pybra.tictoc import Timer
 
 fontsize = 12
 plt.rc('font', family='serif')
@@ -23,7 +19,7 @@ maxSpeed=1.03
 levelsLines=np.sort([1.05,1.0,0.99,0.98,0.95,0.9,0.5])
 
 # ---
-ny=100 # 200
+ny=30 # 200
 nx=ny*4
 
 input_file="Layout_1x2.json"
@@ -43,42 +39,32 @@ titles.append('FLORIS (with induction')
 
 # --- Parametric computatoin
 if bCompute:
-    with Timer('time'):
-        planes=[]
+    planes=[]
+    print('-----------------------------------------------------------------')
+    Ind_Opts['no_induction']=True
+    planes.append(get_HH_plane_vel(input_file, Ind_Opts, resolution, bounds_to_set=bounds))
+    savePlane(planes[-1],'_data/Layout12_0_',U0=U0)
 
-        print('-----------------------------------------------------------------')
-        Ind_Opts['no_induction']=True
-        planes.append(get_HH_plane_vel(input_file, Ind_Opts, resolution, bounds_to_set=bounds))
-        savePlane(planes[-1],'_data/Layout12_0_',U0=U0)
-
-        print('-----------------------------------------------------------------')
-        Ind_Opts['no_induction']=False
-        Ind_Opts['blend']=False
-        planes.append(get_HH_plane_vel(input_file, Ind_Opts, resolution, bounds_to_set=bounds))
-        savePlane(planes[-1],'_data/Layout12_1_',U0=U0)
-
-
-#     Ind_Opts['no_induction']=False
-#     Ind_Opts['blend']=True
-#     planes.append(get_HH_plane_vel(input_file, Ind_Opts, resolution, bounds_to_set=bounds))
-#     savePlane(planes[-1],'_data/Layout12_2_'.format(i),U0=U0)
+    print('-----------------------------------------------------------------')
+    Ind_Opts['no_induction']=False
+    Ind_Opts['blend']=False
+    planes.append(get_HH_plane_vel(input_file, Ind_Opts, resolution, bounds_to_set=bounds))
+    savePlane(planes[-1],'_data/Layout12_1_',U0=U0)
 
 planes=[]
 planes.append(loadPlane('_data/Layout12_0_'))
 planes.append(loadPlane('_data/Layout12_1_'))
-# planes.append(loadPlane('_data/Layout12_2_'))
 
 
-# Plot and show
-cmap,valuesOri=get_cmap(minSpeed,maxSpeed)
-fig, axes = plt.subplots(nrows=len(titles), ncols=1, sharex=True, sharey=True, figsize=(6.0*3.6, 6.0))
+# --- Plot and show
+fig, axes = plt.subplots(nrows=len(titles), ncols=1, sharex=True, sharey=True, figsize=(12.0, 6.0))
 for i,(ax,p,t) in enumerate(zip(axes.flat,planes,titles)):
     x=p[0]
     y=p[1]
     u=p[2]
     v=p[3]
     im = plotPlane(x/D,y/D,u,v,ax,minSpeed=minSpeed,maxSpeed=maxSpeed,
-            nStreamlines=nStreamlines,levelsLines=levelsLines, cmap=cmap, axial=True, colors='k')
+            nStreamlines=nStreamlines,levelsLines=levelsLines, axial=True, colors='k')
     ax.title.set_text(t)
 
     ax.set_ylabel('r/D [-]')
@@ -101,10 +87,6 @@ if bColorBar:
 else:
     fig.subplots_adjust(left=0.035, right=0.990, top=0.96, bottom=0.08,hspace=0.17)
 
-if bExport:
-    fig.savefig('Layout12.pdf',bbox_to_inches='tight',dpi=300)
-        # fig.savefig('Layout12.png',bbox_to_inches='tight',dpi=600)
-else:
-    plt.show()
+plt.show()
 
 # 
--- coupling_examples/helper_functions.py
+++ coupling_examples/helper_functions.py
@@ -10,55 +10,6 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import floris.tools as wfct
 from floris.utilities import Vec3
 
-try:
-    from pybra.colors import *
-    from pybra.lic import lic, licImage
-    from pybra.curves import streamQuiver
-    from pybra.clean_exceptions import *
-except:
-    raise Exception('This script requires the package `pybra` from https://github.com/ebranlard/pybra/')
-
-def get_cmap(minSpeed,maxSpeed):
-    DS=0.001
-    # MathematicaDarkRainbow=[(60 /255,86 /255,146/255), (64 /255,87 /255,142/255), (67 /255,107/255,98 /255), (74 /255,121/255,69 /255), (106/255,141/255,61 /255), (159/255,171/255,67 /255), (207/255,195/255,77 /255), (223/255,186/255,83 /255), (206/255,128/255,76 /255), (186/255,61 /255,58 /255)]
-
-    #     ManuDarkOrange  = np.array([198 ,106,1   ])/255.;
-    #     ManuLightOrange = np.array([255.,212,96  ])/255.;
-    # (1,212/255,96/255),  # Light Orange
-    # (159/255,159/255,204/255), # Light Blue
-    #     MathematicaLightGreen = np.array([158,204,170 ])/255.;
-    # (159/255,159/255,204/255), # Light Blue
-    seq=[
-    (63/255 ,63/255 ,153/255), # Dark Blue
-    (159/255,159/255,204/255), # Light Blue
-    (158/255,204/255,170/255), # Light Green
-    (1,212/255,96/255),  # Light Orange
-    (1,1,1),  # White
-    (1,1,1),  # White
-    (1,1,1),  # White
-    (138/255 ,42/255 ,93/255), # DarkRed
-    ]
-    valuesOri=np.array([
-    minSpeed,  # Dark Blue
-    0.90,
-    0.95,
-    0.98,
-    1.00-DS , # White
-    1.00    , # White
-    1.00+DS , # White
-    maxSpeed         # DarkRed
-    ])
-    values=(valuesOri-min(valuesOri))/(max(valuesOri)-min(valuesOri))
-
-    valuesOri=np.around(valuesOri[np.where(np.diff(valuesOri)>DS)[0]],2)
-
-    cmap= make_colormap(seq,values=values)
-    return cmap,np.concatenate((valuesOri,[maxSpeed]))
-
-
-
-
-
 def get_HH_plane_vel(input_file, Ind_Opts, resolution=Vec3(232, 114, 3),bounds_to_set=None):
     fi = wfct.floris_utilities.FlorisInterface(input_file)
     # Calculate wake
@@ -66,7 +17,7 @@ def get_HH_plane_vel(input_file, Ind_Opts, resolution=Vec3(232, 114, 3),bounds_t
 
     #fd=fi.get_flow_data(resolution = resolution, Ind_Opts=Ind_Opts)
     
-    # # Initialize the horizontal cut
+    # Initialize the horizontal cut
     hor_plane = wfct.cut_plane.HorPlane(
         fi.get_flow_data(resolution = resolution, Ind_Opts=Ind_Opts, bounds_to_set=bounds_to_set),
         fi.floris.farm.turbines[0].hub_height
@@ -119,22 +70,15 @@ def plotPlane(x,y,u,v,ax,minSpeed=None,maxSpeed=None, cmap='coolwarm', colors='w
 
     # Plot the cut-through
     im = ax.pcolormesh(x, y, Z, cmap=cmap, vmin=minSpeed, vmax=maxSpeed)
-    #im = ax.contourf  (x, y, Z, levels=levelsContour, cmap=cmap, vmin=minSpeed, vmax=maxSpeed)
-
     if levelsLines is not None:
         rcParams['contour.negative_linestyle'] = 'solid'
         cs=ax.contour(x, y, Z, levels=levelsLines, colors=colors, linewidths=linewidths, alpha=alpha)
-#         ax.clabel(cs,list(levelsLines))
 
     if nStreamlines>0:
         yseed=np.linspace(min(y)*0.9,max(y)*0.9,nStreamlines)
         start=np.array([yseed*0,yseed])
         sp=ax.streamplot(x,y,u,v,color='k',start_points=start.T,linewidth=0.7,density=30,arrowstyle='-')
-#         sp=ax.streamplot(x,y,u,v,color='k',linewidth=0.7,density=2)
-        qv=streamQuiver(ax,sp,n=5,scale=40,angles='xy')
-
-
-
+#         qv=streamQuiver(ax,sp,n=5,scale=40,angles='xy')
 
     ax.set_aspect('equal')
 
